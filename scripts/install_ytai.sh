79c17f3175cd2802b6ed4a1aef97c78521b6b556
iamthonifhomuhali <muhali.labs@gmail.com>
2026-02-23 10:33:30 +0200

    Update install_ytai.sh to include requirements handling
    Added installation of requirements and cleanup steps.

--- scripts/install_ytai.sh
+++ scripts/install_ytai.sh
@@ -3,7 +3,11 @@ pkg update -y
 pkg install python ffmpeg git -y
 pip install yt-dlp google-generativeai scenedetect[opencv] tqdm python-dotenv faiss-cpu
 mkdir -p $PREFIX/bin
+pip install reqs
 curl -L -o ytai.py https://github.com/Media-CLis/YTAI-CLi/raw/refs/heads/main/cli/ytai.py
 chmod +x ytai.py
+reqs . > requirements.txt 
+pip install -r requirements.txt 
+rm requirements.txt 
 mv ytai.py $PREFIX/bin/ytai
 echo "YTAI installed. Run with: ytai download --url <playlist_url>"
